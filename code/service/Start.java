b52118f50476aba1ea2026070bc06a08e7c35cd6
Voragun <voragun2543@gmail.com>
2023-04-27 19:55:43 +0700

    Setup project and load a webflux dependency

--- code/service/Start.java
+++ code/service/Start.java
@@ -1,7 +1,27 @@
 package service;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 class Start {
     public static void main(String[] data) {
-        System.out.println("Hello");
+        ApplicationContext context;
+        context = SpringApplication.run(Setup.class);
+    }
+}
+
+@SpringBootApplication
+class Setup {
+    
+}
+
+@RestController
+class Sample {
+    @RequestMapping("/check")
+    double check() {
+        return 3.14159367;
     }
 }
